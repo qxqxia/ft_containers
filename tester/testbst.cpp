534547d3ea9a3360c833ddf3da523ab644feb27b
qin xia <qinxia@macbook-air-de-qin.home>
2023-01-11 22:54:28 +0100

    testbst

---
+++ tester/testbst.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+using namespace std;
+
+struct BstNode
+{
+    int data;
+    BstNode *left;
+    BstNode *right;
+};
+
+BstNode *GetNewNode(int data)
+{
+    BstNode *newNode = new BstNode();
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+BstNode *Insert(BstNode *root, int data)
+{
+    if (root == NULL)
+    {
+        root = GetNewNode(data);
+        return root;
+    }
+    else if (data <= root->data)
+    {
+        root->left = Insert(root->left, data);
+    }
+    else
+    {
+        root->right = Insert(root->right, data);
+    }
+    return root;
+}
+
+bool Search(BstNode *root, int data)
+{
+    if (root == NULL)
+        return false;
+    else if (root->data == data)
+        return true;
+    else if (data <= root->data)
+        return Search(root->left, data);
+    else
+        return Search(root->right, data);
+}
+
+void inorder(BstNode *root)
+{
+    if (root)
+    {
+        inorder(root->left);
+        std::cout << root->data << " ";
+        inorder(root->right);
+    }
+}
+
+void preorder(BstNode *root)
+{
+    if (root)
+    {
+        std::cout << root->data << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(BstNode *root)
+{
+    if (root)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        std::cout << root->data << " ";
+    }
+}
+
+int main()
+{
+    BstNode *root = NULL;
+    root = Insert(root, 15);
+    root = Insert(root, 10);
+    root = Insert(root, 20);
+    root = Insert(root, 25);
+    root = Insert(root, 8);
+    root = Insert(root, 12);
+    inorder(root);
+    cout << endl;
+    preorder(root);
+    cout << endl;
+    postorder(root);
+    cout << endl;
+    int number;
+    cout << "Enter number be serached\n";
+    cin >> number;
+    if (Search(root, number) == true)
+        cout << "Found\n";
+    else
+        cout << "Not Found\n";
+}
