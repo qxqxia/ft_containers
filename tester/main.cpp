572f0c6a25adf40d8b72e067b89ef7cb4991b5a4
Qin XIA <qxia@e1r9p6.clusters.42paris.fr>
2023-02-01 16:02:39 +0100

    tester/vector

---
+++ tester/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "vector.hpp"
+
+int main(){
+    ft::vector(int) v1;
+
+    if (v1.empty()){
+        std::cout << "size is 0.\n"; 
+    }
+    std::cout << "test compile.\n";
+}
